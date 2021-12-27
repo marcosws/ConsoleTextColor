2ed6d314c1870034256c9adb27167a6cd254754a
Marcos <marcosws@ymail.com>
2021-12-27 12:40:54 -0300

    ajustes no javadoc

--- src/com/github/marcosws/console/ConsoleTextColor.java
+++ src/com/github/marcosws/console/ConsoleTextColor.java
@@ -9,11 +9,6 @@ import com.github.marcosws.console.enums.ForegroundStyle;
 /**
  * Class: ConsoleTextColor - Format font color and style in console
  * @author Marcos Souza - www.github.com/marcosws
- * Data: 26/12/2021
- * References:
- * https://ss64.com/nt/syntax-ansi.html - How-to: Use ANSI colors in the terminal
- * https://github.com/mlocati - https://gist.github.com/mlocati/fdabcaeb8071d5c75a2d51712db24011#file-win10colors-cmd
- * https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
  */
 public class ConsoleTextColor {
 
